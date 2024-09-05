f1d9cb672a74649c2319ae95195e1d35d1039d53
dgelashvili <dgela21@freeuni.edu.ge>
2024-09-05 21:31:26 +0400

    Major refactoring. Added main menu logic as well

--- include/Client.h
+++ include/Client.h
@@ -4,6 +4,13 @@
 #include <string>
 #include <winsock2.h>
 
+/**
+* This class is the main class which is responsible for communicating server with 'console'.
+* It consists of three main phases:
+* Authentication: This is where client tries to log in or register
+* Main Menu: This is the main page of Server where client has to decide whether quit or start playing
+* Game: TODO
+*/
 class Client {
 public:
     Client(std::string  ip, int port);
@@ -11,13 +18,15 @@ public:
 
     void connectToServer() const;
     void disconnectFromServer() const;
-
-    void sendToServer(const std::string& message) const;
-    [[nodiscard]] std::string receiveFromServer() const;
+    [[nodiscard]] std::string enterCommand() const;
+    [[nodiscard]] bool authenticationPhase() const;
+    [[noreturn]] void mainMenuPhase() const;
 
 private:
     static void initializeWinSock();
     void setupConnectingSocket();
+    void sendToServer(const std::string& message) const;
+    [[nodiscard]] std::string receiveFromServer() const;
 
     std::string ip;
     int port;
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,12 +1,15 @@
 #include "Client.h"
 #include <iostream>
+#include <thread>
 
+//starts initializing WinSock and setups 'connectingSocket'
 Client::Client(std::string ip, const int port)
     : ip(std::move(ip)), port(port), connectSocket(INVALID_SOCKET) {
     initializeWinSock();
     setupConnectingSocket();
 }
 
+//calls 'disconnectFromServer()' which handles clearing everything
 Client::~Client() {
     disconnectFromServer();
 }
@@ -27,20 +30,21 @@ void Client::setupConnectingSocket() {
     }
 }
 
+// Sets up connection with the server
 void Client::connectToServer() const {
     sockaddr_in clientService{};
     clientService.sin_family = AF_INET;
     clientService.sin_addr.s_addr = inet_addr(ip.c_str());
     clientService.sin_port = htons(port);
 
-    if (connect(connectSocket, reinterpret_cast<SOCKADDR *>(&clientService), sizeof(clientService)) == SOCKET_ERROR) {
+    if (connect(connectSocket, (SOCKADDR*)(&clientService), sizeof(clientService)) == SOCKET_ERROR) {
         const auto err = WSAGetLastError();
         disconnectFromServer();
         throw std::runtime_error("connection failed: " + std::to_string(err));
     }
 }
 
-
+// Closes 'connectSocket' if one is valid and also cleans up WSA
 void Client::disconnectFromServer() const {
     if (connectSocket != INVALID_SOCKET) {
         closesocket(connectSocket);
@@ -48,10 +52,12 @@ void Client::disconnectFromServer() const {
     WSACleanup();
 }
 
+// Wrapper of send method
 void Client::sendToServer(const std::string &message) const {
-    send(connectSocket, message.c_str(), static_cast<int>(message.size()), 0);
+    send(connectSocket, message.c_str(), (int)(message.size()), 0);
 }
 
+// Wrapper of receive method
 std::string Client::receiveFromServer() const {
     char buffer[512];
     if (const int bytesReceived = recv(connectSocket, buffer, sizeof(buffer) - 1, 0); bytesReceived > 0) {
@@ -61,3 +67,81 @@ std::string Client::receiveFromServer() const {
     }
     return "";
 }
+
+/**
+* This method is called when user has to enter some kind of command after which
+* the page is going to refresh.
+*
+* Returns last message from the server
+*/
+std::string Client::enterCommand() const {
+    std::string serverMessage = receiveFromServer();
+    std::string userInput;
+    if (serverMessage.empty()) {
+        return "";
+    }
+    std::cout << serverMessage;
+    std::cin >> userInput;
+    sendToServer(userInput);
+
+    system("cls");
+
+    serverMessage = receiveFromServer();
+    std::cout << serverMessage;
+
+    return serverMessage;
+}
+
+
+// Handles authentication pages and communication
+bool Client::authenticationPhase() const {
+    while(true) {
+        //Enter command (REG/LOG/EXIT):
+        std::string serverMessage = enterCommand();
+        if (serverMessage == "Goodbye!" || serverMessage.empty()) {
+            break;
+        }
+
+        //REG or LOG phase
+        while (serverMessage.find("Enter") != std::string::npos ||
+                serverMessage.find("Repeat") != std::string::npos) {
+            std::string userInput;
+            std::cin >> userInput;
+            sendToServer(userInput);
+            serverMessage = receiveFromServer();
+            std::cout << serverMessage;
+        }
+
+        //Additional message from server
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        system("cls");
+        if (serverMessage.find("logged in successfully") != std::string::npos) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Handles main menu page and communication
+[[noreturn]] void Client::mainMenuPhase() const {
+    while(true) {
+        //play or exit (P/X):
+        std::string serverMessage = enterCommand();
+        if (serverMessage == "Goodbye!"  || serverMessage.empty()) {
+            break;
+        }
+
+        //response of server: either retrying or playing
+        if (serverMessage.find("unknown") != std::string::npos) {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            system("cls");
+        } else {
+            while (true) {
+
+            }
+        }
+    }
+}
+
+
